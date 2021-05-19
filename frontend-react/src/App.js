91a46861fa44225754aeb07d89f32b226b128575
raed.abusanad <raed.abusanad@hotmail.com>
2021-05-19 12:45:01 +0100

    img

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import logo from './logo.svg';
 import Connex from '@vechain/connex'
 
 const myAccountAddress = "0x46E305b4E1883a5f1f104464dcEdcbd2618D9EA9";
@@ -15,11 +16,14 @@ function App() {
 	const [name, setName] = useState("");
 	const [result, setResult] = useState("");
 	const [accInfo, setAccInfo] = useState({});
+	const [contractInfo, setContractInfo] = useState({});
 	const [connex, setConnex] = useState(null);
 	const [account, setAccount] = useState({});
 	const [loading, setLoading] = useState(false)
 
 	const initialise = async () => {
+
+		setLoading(true);
 
 		try {
 			const connex = new Connex({
@@ -34,11 +38,18 @@ function App() {
 			let status = connex.thor.status;
 			console.log("status:", status);
 
+			//const contractAccount = connex.thor.account(contractAddress);
+			//const contractInfo = await contractAccount.get();
+			//console.log("contractAccount:", contractAccount);
+
 			setAccount(account);
+			setContractInfo(contractInfo);
 			setAccInfo(info);
 		} catch (err) {
 			console.error(err);
 		}
+
+		setLoading(false);
 	}
 
 	useEffect(() => {
@@ -57,11 +68,11 @@ function App() {
 
 	return (
 		<div className="App">
-			{/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header> */}
 			<h2>Account Info</h2>
 			<div>{JSON.stringify(accInfo)}</div>
+
+			<h2>Contract Info</h2>
+			<div>{JSON.stringify(contractInfo)}</div>
 
 			<h2>Write operation</h2>
 			<h4>Requires transaction to be signed your using private key and VTHO availability.</h4>
@@ -79,6 +90,12 @@ function App() {
 				Read
       		</button>
 			<div>{result}</div>
+
+			{loading &&
+				<div>
+					<img src={logo} className="App-logo" alt="logo" />
+				</div>
+			}
 		</div>
 	);
 }
