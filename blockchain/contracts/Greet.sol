dba3caded9c6e7642ec8dc3101606a981e54e9ff
raed.abusanad <raed.abusanad@hotmail.com>
2021-05-19 10:02:19 +0100

    frontend

--- blockchain/contracts/Greet.sol
+++ blockchain/contracts/Greet.sol
@@ -2,14 +2,13 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Greet {
-
     string user;
 
     function store(string memory name) public {
         user = name;
     }
 
-    function hello() public view returns (string memory){
+    function hello() public view returns (string memory) {
         return user;
     }
 }
