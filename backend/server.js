2a550e5a1092d7b10cefb1de5067741ee2f7d53c
raed.abusanad <raed.abusanad@hotmail.com>
2021-05-19 15:10:00 +0100

    import { Transaction, secp256k1 } from 'thor-devkit'

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const connex = new Connex({
     network: 'test'
 });
 
+
+console.log("status:", connex.thor.status);
+
 const myAccountAddress = "0x46E305b4E1883a5f1f104464dcEdcbd2618D9EA9";
 
 const contractAddress = "0xed5D02D6f6Ee48C29F003B4B99F7c19EC75Ba3a6";
@@ -14,6 +17,10 @@ const account = connex.thor.account(myAccountAddress);
 
 const method = account.method(contract[1]);
 
-/** call the method (dry-run, without altering blockchain) */
-let result = await method.call();
-console.log("result:", result.decoded);
+(async () => {
+
+    /** call the method (dry-run, without altering blockchain) */
+    let result = await method.call();
+    console.log("result:", result.decoded);
+
+})();
